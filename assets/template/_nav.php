83862f2e6a0fbca38bdd6e7446b3c98d497e6238
DonovanDumeige <98708424+DonovanDumeige@users.noreply.github.com>
2022-09-15 16:42:19 +0200

    Ajout des template PHP

---
+++ assets/template/_nav.php
@@ -0,0 +1,5 @@
+<div class="menu-burger">
+    <div class="first-bar"></div>
+    <div class="second-bar"></div>
+    <div class="third-bar"></div>
+</div>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$title = "acceuil";
+
+require("./assets/template/_header.php");
+?>
+
+<p>Bonjour</p>
+<?php 
+require("./assets/template/_footer.php");
+?>
