f9db00392ebb2d727f497263fb35df506edcbbbc
Kendra <you@example.com>
2022-09-19 16:56:48 +0200

    ByKendra

---
+++ pages/start.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="/assets/style/style.css">
+<main id="startMain">
+    <button id="contactSupport">Contacter le support</button>
+    <div id="startBox">
+    <h1>Interact'Lille</h1>
+    <p>Découvrir la ville autrement</p>
+    <button id="playButton">Jouer</button>
+    </div>
+    <a href="" id="howToPlay">Comment jouer ?</a>
+</main>
