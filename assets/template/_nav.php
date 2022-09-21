e9af0d9e4b0bb4b312422269e69092038c71eab3
Kendra <you@example.com>
2022-09-21 12:56:52 +0200

    Kendra

---
+++ assets/template/_nav.php
@@ -0,0 +1,15 @@
+
+<div class="menu">
+    <div class="menu-burger">
+    <i class="fa-solid fa-bars"></i>
+    </div>
+    <nav class="navbar">
+        <ul>
+            <li><a href="#">Lieux et monuments</a></li>
+            <li><a href="#">Histoire de Lille</a></li>
+            <li><a href="#">Parcs et jardins</a></li>
+            <li><a href="#">Comment jouer ?</a></li>
+            <li><a href="#">Contacter le support</a></li>
+        </ul>
+    </nav>
+</div>
--- pages/screenPlayQuizz.php
+++ pages/screenPlayQuizz.php
@@ -1,27 +1,38 @@
-
-<!-- require "template/_nav"; -->
-<link rel="stylesheet" href="/assets/style/style.css">
+<?php require "../assets/template/_header.php"?>
 <main id="screenPlayQuizzMain">
+<?php require "../assets/template/_nav.php"?>
 <div id="topPart">
+        <div id="containerTitles">
     <h1>La catégorie</h1>
     <p>La question : blah blah blah ?</p>
     <h2>La sous-catégorie</h2>
+        
+    </div>
 </div>
 
-
-<div id="bottomPart">
 <article id="visitPlace">
-    <p></p>
-    <p></p>
-    <p></p>
-    <a href="">Voir sur la carte</a>
+    <div id="infoVisitPlace">
+        <h3>Visitez cet endroit</h3>
+        <ul>
+            <li>Adresse : 666 rue de l'enfer, ResponsiveDeLaMuerte</li>
+            <li>Telephone : xx xx xx xx xx</li>
+            <li>À proximité de : ton Âme</li>
+            <li>Temps de trajet : Une vie entière Bro</li>
+        </ul>
+
+    </div>
+    <a href="">Voir sur la carte</a> 
+    <div id="buttonVisitPlace">Visitez cet endroit</div>
 </article>
+<div id="bottomPart">
+
 
 <article id="propositions">
-<button class="pick"></button>
-<button class="pick"></button>
-<button class="pick"></button>
-<button class="pick"></button>
+<button class="pick">Réponse : A</button>
+<button class="pick">Réponse : B</button>
+<button class="pick">Réponse : C</button>
+<button class="pick">Réponse : D</button>
 </article>
 </div>
 </main>
+<?php require "../assets/template/_footer.php"?>
